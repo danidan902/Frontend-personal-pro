8f0eb678aa5f21b63b36932b2e85d3a4069fe9a4
danidan <danid93575@gmail.com>
2025-07-12 11:55:06 -0700

    project

--- src/LoginRegister.jsx
+++ src/LoginRegister.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import axios from 'axios'
+import './project.css'
 function LoginRegister() {
   const [name, setName] = useState("")
   const [password, setPassword] = useState("")
